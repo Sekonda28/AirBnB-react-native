15c9ee86da293e6eb370678e381b107f25a6896b
Matt Caswell <matt.caswell@hotmail.com>
2021-11-23 17:50:05 +0100

    sign in and sign up screens added

--- containers/SignInScreen.js
+++ containers/SignInScreen.js
@@ -1,66 +1,155 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigation } from "@react-navigation/core";
-import { Button, Text, TextInput, View, TouchableOpacity, Alert } from "react-native";
+import {
+  StyleSheet,
+  Button,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+  Alert,
+  Image,
+} from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 export default function SignInScreen({ setToken }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [pWordHidden, setPWordHidden] = useState(true);
   const navigation = useNavigation();
-  console.log(email, password);
 
-  const handlePress = async()=>{
+  const handlePress = async () => {
     try {
-          if(password !== ""|| email !==""){ 
-        const response = await axios.post("https://express-airbnb-api.herokuapp.com/user/log_in", {
-          "email": email,
-          "password": password,
-        })
+      if (password !== "" || email !== "") {
+        const response = await axios.post(
+          "https://express-airbnb-api.herokuapp.com/user/log_in",
+          {
+            email: email,
+            password: password,
+          }
+        );
         //setUserId(response.data.username);
         setToken(response.data.token);
-        
-       
-    } else {
-      Alert.alert("Please enter an email and a password")
-    }
+      } else {
+        Alert.alert("Please enter an email and a password");
+      }
     } catch (error) {
-      console.log(error.message)
-      {Alert.alert("Username or password incorrect")}
+      console.log(error.message);
+      {
+        Alert.alert("Username or password incorrect");
+      }
     }
-
-  }
+  };
   return (
-    <View>
+    <KeyboardAwareScrollView>
       <View>
-      <KeyboardAwareScrollView>
-        <Text>Name: </Text>
-        <TextInput
-          placeholder="Username"
-          onChangeText={(text) => {
-            setEmail(text);
-          }}
-        />
-        <Text>Password: </Text>
-        <TextInput
-          placeholder="Password"
-          secureTextEntry={true}
-          onChangeText={(text) => {
-            setPassword(text);
-          }}
-        />
-        <Button
-          title="Sign in"
-          onPress={handlePress}
-        />
-        <TouchableOpacity
-          onPress={()=>{
-            navigation.navigate("SignUp")
-          }}
-        >
-          <Text>Create an account</Text>
-        </TouchableOpacity></KeyboardAwareScrollView>
+        <View style={styles.pageContainer}>
+          <View style={styles.headerContainer}>
+            <Image
+              style={styles.logo}
+              source={require("../assets/airbnb-logo.png")}
+              resizeMode="contain"
+            />
+            <Text style={styles.headerText}>Sign In</Text>
+          </View>
+
+          <View style={styles.inputBorder}>
+            <TextInput
+              style={styles.inputLine}
+              placeholder="email"
+              onChangeText={(text) => {
+                setEmail(text);
+              }}
+            />
+          </View>
+          <View style={styles.inputBorder}>
+            <TextInput
+              style={styles.inputLine}
+              placeholder="password"
+              secureTextEntry={pWordHidden}
+              onChangeText={(text) => {
+                setPassword(text);
+              }}
+            />
+            <Text
+              onPress={() => {
+                setPWordHidden(!pWordHidden);
+              }}
+            >
+              👁
+            </Text>
+          </View>
+
+          <TouchableOpacity style={styles.button} onPress={handlePress}>
+            <Text style={styles.buttonText}>Sign in</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate("SignUp");
+            }}
+          >
+            <Text style={styles.accountText}>No account ? Register</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </KeyboardAwareScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  pageContainer: {
+    marginLeft: 30,
+    marginRight: 30,
+  },
+
+  headerContainer: {
+    height: 300,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logo: {
+    height: 100,
+    width: 100,
+  },
+  headerText: {
+    fontSize: 20,
+    color: "#767676",
+    fontWeight: "700",
+    paddingTop: 30,
+  },
+
+  inputLine: {
+    paddingBottom: 5,
+  },
+  inputBorder: {
+    borderBottomColor: "#FF5A5F",
+    borderBottomWidth: 1,
+    marginBottom: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  button: {
+    backgroundColor: "#fff",
+    width: 150,
+    alignSelf: "center",
+    borderWidth: 2,
+    borderColor: "#FF5A5F",
+    alignItems: "center",
+    borderRadius: 20,
+    marginTop: 60,
+  },
+  buttonText: {
+    color: "#767676",
+    paddingTop: 10,
+    paddingBottom: 10,
+    fontWeight: "700",
+  },
+  accountText: {
+    color: "#767676",
+    paddingTop: 10,
+    paddingBottom: 10,
+    textAlign: "center",
+  },
+});
--- containers/SignUpScreen.js
+++ containers/SignUpScreen.js
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
-import { Alert, Button, Text, TextInput, View } from "react-native";
+import {
+  StyleSheet,
+  Alert,
+  TouchableOpacity,
+  Text,
+  TextInput,
+  View,
+  Image,
+} from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import axios from "axios";
 
@@ -9,6 +17,9 @@ export default function SignUpScreen({ setToken }) {
   const [description, setDescription] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPW, setConfirmPW] = useState("");
+  const [pWordHidden, setPWordHidden] = useState(true);
+  const [confirmPWHidden, setConfirmPWHidden] = useState(true);
+  console.log(email, password, description, confirmPW, username);
 
   const handlePress = async () => {
     try {
@@ -31,8 +42,9 @@ export default function SignUpScreen({ setToken }) {
           );
           //setUserId(response.data.username);
           setToken(response.data.token);
+          console.log(response.data);
         } else {
-          Alert.alert("Your passwords do not match - please try again")
+          Alert.alert("Your passwords do not match - please try again");
         }
       } else {
         Alert.alert("Please make sure all the fields are filled in");
@@ -46,29 +58,154 @@ export default function SignUpScreen({ setToken }) {
   };
 
   return (
-    <View>
+    <KeyboardAwareScrollView>
       <View>
-        <KeyboardAwareScrollView>
-          <Text>email: </Text>
-          <TextInput placeholder="email" onChangeText = {(text)=>setUsername(text)} />
-          <Text>username: </Text>
-          <TextInput placeholder="username" onChangeText={(text)=>setUsername(text)}/>
-          <Text>description: </Text>
-          <TextInput
-            numberOfLines={4}
-            placeholder="Describe yourself in a few words"
-            onChangeText={(text)=>{setDescription(text)}}
-          />
-          <Text>Password: </Text>
-          <TextInput placeholder="password" secureTextEntry={true} onChangeText={(text)=>setPassword(text)}/>
-          <Text>Confirm password: </Text>
-          <TextInput placeholder="confirm password" secureTextEntry={true} on />
-          <Button
-            title="Sign up"
-            onPress={handlePress}
-          />
-        </KeyboardAwareScrollView>
+        <View style={styles.pageContainer}>
+          <View style={styles.headerContainer}>
+            <Image
+              style={styles.logo}
+              source={require("../assets/airbnb-logo.png")}
+              resizeMode="contain"
+            />
+            <Text style={styles.headerText}>Sign In</Text>
+          </View>
+
+          <View>
+            <View style={styles.inputBorder}>
+              <TextInput
+                style={styles.inputLine}
+                placeholder="email"
+                onChangeText={(text) => setEmail(text)}
+              />
+            </View>
+            <View style={styles.inputBorder}>
+              <TextInput
+                style={styles.inputLine}
+                placeholder="username"
+                onChangeText={(text) => setUsername(text)}
+              />
+            </View>
+            <View style={styles.inputBorderDesc}>
+              <TextInput
+                style={styles.inputDesc}
+                numberOfLines={4}
+                placeholder="Describe yourself in a few words"
+                onChangeText={(text) => {
+                  setDescription(text);
+                }}
+              />
+            </View>
+            <View style={styles.inputBorder}>
+              <TextInput
+                style={styles.inputLine}
+                placeholder="password"
+                secureTextEntry={pWordHidden}
+                onChangeText={(text) => setPassword(text)}
+              />
+              <Text
+                onPress={() => {
+                  setPWordHidden(!pWordHidden);
+                }}
+              >
+                👁
+              </Text>
+            </View>
+            <View style={styles.inputBorder}>
+              <TextInput
+                style={styles.inputLine}
+                placeholder="confirm password"
+                secureTextEntry={confirmPWHidden}
+                onChangeText={(text) => setConfirmPW(text)}
+              />
+              <Text
+                onPress={() => {
+                  setConfirmPWHidden(!confirmPWHidden);
+                }}
+              >
+                👁
+              </Text>
+            </View>
+          </View>
+          <TouchableOpacity style={styles.button} onPress={handlePress}>
+            <Text style={styles.buttonText}>Sign up</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate("SignIn");
+            }}
+          >
+            <Text style={styles.accountText}>
+              Already have an account ? Sign in
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </KeyboardAwareScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  pageContainer: {
+    marginLeft: 30,
+    marginRight: 30,
+  },
+
+  headerContainer: {
+    height: 180,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logo: {
+    height: 100,
+    width: 100,
+  },
+  headerText: {
+    fontSize: 20,
+    color: "#767676",
+    fontWeight: "700",
+    paddingTop: 30,
+  },
+
+  inputLine: {
+    paddingBottom: 5,
+  },
+  inputBorder: {
+    borderBottomColor: "#FF5A5F",
+    borderBottomWidth: 1,
+    marginBottom: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  inputBorderDesc: {
+    borderColor: "#FF5A5F",
+    borderWidth: 1,
+    marginBottom: 20,
+  },
+  inputDesc: {
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  button: {
+    backgroundColor: "#fff",
+    width: 150,
+    alignSelf: "center",
+    borderWidth: 2,
+    borderColor: "#FF5A5F",
+    alignItems: "center",
+    borderRadius: 20,
+    marginTop: 60,
+  },
+  buttonText: {
+    color: "#767676",
+    paddingTop: 10,
+    paddingBottom: 10,
+    fontWeight: "700",
+  },
+  accountText: {
+    color: "#767676",
+    paddingTop: 10,
+    paddingBottom: 10,
+    textAlign: "center",
+  },
+});
